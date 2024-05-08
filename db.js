770ecf8354ef9377d375dd1ce9e95748b2391fe8
Shami_Mourya <88420924+ShamiKantMourya@users.noreply.github.com>
2024-05-08 06:18:05 +0000

    database

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URL;
+
+exports.dataBase = () => {
+  mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+};
